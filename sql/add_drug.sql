bdcf5e958f1c359d15d10c5f539d5e3998f0807d
z <zhaohuaxs.sdu@gmail.com>
2022-09-26 21:42:31 +0800

    db

--- sql/add_drug.sql
+++ sql/add_drug.sql
@@ -1,3 +1,7 @@
-INSERT INTO drug (name, drug_id, drug_number, ingredient, character, major_function, specification, usage_dosage, adverse_reaction, taboo, matters_need_attention, store_up, expiry_date, produced_time, approval_number, manufacturing_enterprise)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
+-- INSERT INTO drug (name, drug_id, drug_number, ingredient, character, major_function, specification, usage_dosage, adverse_reaction, taboo, matters_need_attention, store_up, expiry_date, produced_time, approval_number, manufacturing_enterprise)
+-- VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
+-- RETURNING $table_fields;
+
+INSERT INTO drug (name, drug_id, drug_number, usage_dosage, matters_need_attention, a_b_classify)
+VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING $table_fields;
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use tokio_pg_mapper_derive::PostgresMapper;
 use tokio_postgres::error::Error;
+use uuid::Uuid;
 use tokio_postgres::Row;
 use postgres_types::{self, ToSql, FromSql};
 
@@ -10,21 +11,21 @@ pub struct Drug {
     pub name: String,
     pub drug_id: String,
     pub class_id: String,
-    pub drug_number: String,
-    pub ingredient: String,
-    pub character: String,
-    pub major_function: String,
-    pub specification: String,
+    // pub drug_number: String,
+    // pub ingredient: String,
+    // pub character: String,
+    // pub major_function: String,
+    // pub specification: String,
     pub usage_dosage: String,
-    pub adverse_reaction: String,
-    pub taboo: String,
+    // pub adverse_reaction: String,
+    // pub taboo: String,
     pub matters_need_attention: String,
-    pub store_up: String,
-    pub expiry_date: i64,
-    pub produced_time: String,
-    pub approval_number: String,
+    // pub store_up: String,
+    // pub expiry_date: i64,
+    // pub produced_time: String,
+    // pub approval_number: String,
     pub a_b_classify: String,
-    pub manufacturing_enterprise: String,
+    // pub manufacturing_enterprise: String,
 }
 
 impl Drug {
@@ -32,23 +33,23 @@ impl Drug {
         println!("my from_row_ref");
         Ok(Drug{
             name: row.try_get::<&str, String>("name")?.to_string(),
-            drug_id: row.try_get::<&str, postgres_types::Type::UUID>("drug_id")?.to_string(),
-            class_id: row.try_get::<&str, postgres_types::Type::UUID>("class_id")?.to_string(),
-            drug_number: row.try_get::<&str, String>("drug_number")?.to_string(),
-            ingredient: row.try_get::<&str, String>("ingredient")?.to_string(),
-            character: row.try_get::<&str, String>("character")?.to_string(),
-            major_function: row.try_get::<&str, String>("major_function")?.to_string(),
-            specification: row.try_get::<&str, String>("specification")?.to_string(),
+            drug_id: row.try_get::<&str, Uuid>("drug_id")?.hyphenated().to_string(),
+            class_id: row.try_get::<&str, Uuid>("class_id")?.hyphenated().to_string(),
+            // drug_number: row.try_get::<&str, String>("drug_number")?.to_string(),
+            // ingredient: row.try_get::<&str, String>("ingredient")?.to_string(),
+            // character: row.try_get::<&str, String>("character")?.to_string(),
+            // major_function: row.try_get::<&str, String>("major_function")?.to_string(),
+            // specification: row.try_get::<&str, String>("specification")?.to_string(),
             usage_dosage: row.try_get::<&str, String>("usage_dosage")?.to_string(),
-            adverse_reaction: row.try_get::<&str, String>("adverse_reaction")?.to_string(),
-            taboo: row.try_get::<&str, String>("taboo")?.to_string(),
+            // adverse_reaction: row.try_get::<&str, String>("adverse_reaction")?.to_string(),
+            // taboo: row.try_get::<&str, String>("taboo")?.to_string(),
             matters_need_attention: row.try_get::<&str, String>("matters_need_attention")?.to_string(),
-            store_up: row.try_get::<&str, String>("store_up")?.to_string(),
-            expiry_date: row.try_get::<&str, i64>("expiry_date")?,
-            produced_time:  row.try_get::<&str, String>("produced_time")?.to_string(),
-            approval_number: row.try_get::<&str, String>("approval_number")?.to_string(),
+            // store_up: row.try_get::<&str, String>("store_up")?.to_string(),
+            // expiry_date: row.try_get::<&str, i64>("expiry_date")?,
+            // produced_time:  row.try_get::<&str, String>("produced_time")?.to_string(),
+            // approval_number: row.try_get::<&str, String>("approval_number")?.to_string(),
             a_b_classify: row.try_get::<&str, String>("a_b_classify")?.to_string(),
-            manufacturing_enterprise: row.try_get::<&str, String>("manufacturing_enterprise")?.to_string()
+            // manufacturing_enterprise: row.try_get::<&str, String>("manufacturing_enterprise")?.to_string()
         })
     }
 }
