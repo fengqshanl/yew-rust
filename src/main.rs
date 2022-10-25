bcf27af913e942294e09aa79c71591494c817ba6
z <zhaohuaxs.sdu@gmail.com>
2022-10-25 19:06:36 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ mod errors;
 mod handlers;
 mod models;
 
-use ::config::Config;
-use actix_web::{App, web, HttpServer};
-use dotenv::dotenv;
-use actix_cors::Cors;
-use handlers::{ drug::{add_drug, get_drug}, sale::{add_sale, get_sale}} ;
-use tokio_postgres::NoTls;
 use crate::config::config::ExampleConfig;
+use ::config::Config;
+use actix_cors::Cors;
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use handlers::{
+    drug::{add_drug, get_drug},
+    sale::{add_sale, get_sale},
+};
+use tokio_postgres::NoTls;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -23,26 +26,25 @@ async fn main() -> std::io::Result<()> {
 
     let config: ExampleConfig = config_.try_deserialize().unwrap();
 
-    println!("config: {:?}, default: {:?}", config, ::config::Environment::default());
-
     let pool = config.pg.create_pool(None, NoTls).unwrap();
 
     let server = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .wrap(Cors::permissive())
-            .service(web::resource("/drug")
-                .route(web::post().to(add_drug))
-                .route(web::get().to(get_drug))
+            .service(
+                web::resource("/drug")
+                    .route(web::post().to(add_drug))
+                    .route(web::get().to(get_drug)),
             )
-            .service(web::resource("/sale")
-                .route(web::post().to(add_sale))
-                .route(web::get().to(get_sale))
+            .service(
+                web::resource("/sale")
+                    .route(web::post().to(add_sale))
+                    .route(web::get().to(get_sale)),
             )
     })
     .bind(config.server_addr.clone())?
     .run();
-    println!("Server running at http://{}/", config.server_addr);
 
     server.await
 }
